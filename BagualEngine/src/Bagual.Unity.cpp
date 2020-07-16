fff0befc315d737d787072273bf08e42dfce7535
Vinicius Santos <vinicius-rs@hotmail.com.br>
2020-07-16 03:10:09 +0300

    Added: Unity build configuration

---
+++ BagualEngine/src/Bagual.Unity.cpp
@@ -0,0 +1,20 @@
+
+#if BGL_UNITY_BUILD == 1
+
+#include "Camera.cpp"
+#include "CameraManager.cpp"
+#include "Canvas.cpp"
+#include "GraphicsDriverBase.cpp"
+#include "GraphicsDriverGeneric.cpp"
+#include "GraphicsPlatform.cpp"
+#include "Viewport.cpp"
+#include "Draw.cpp"
+#include "EngineTests.cpp"
+#include "Logger.cpp"
+#include "Module.cpp"
+#include "PlatformBase.cpp"
+#include "PlatformGeneric.cpp"
+#include "Engine.cpp"
+#include "Settings.cpp"
+
+#endif
