d0544e4b78e94b4db0adef4bda8fb613f0a04bf4
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2023-02-10 01:31:34 +0200

    Updating premake5 version to work with VS2022

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -8,18 +8,21 @@ ECHO.
 
 IF NOT EXIST "premake5.exe" (
 	ECHO Downloading Premake5.zip ...
-	POWERSHELL -Command "Invoke-WebRequest https://github.com/premake/premake-core/releases/download/v5.0.0-alpha15/premake-5.0.0-alpha15-windows.zip -OutFile premake-5.0.0-alpha15-windows.zip"
+	POWERSHELL -Command "Invoke-WebRequest https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-windows.zip -OutFile premake-5.0.0-beta2-windows.zip"
 	ECHO Extracting Premake5.exe ...
-	POWERSHELL -Command "expand-archive -path 'premake-5.0.0-alpha15-windows.zip' -destinationpath '.\'"
+	POWERSHELL -Command "expand-archive -path 'premake-5.0.0-beta2-windows.zip' -destinationpath '.\'"
 	ECHO Deleting cached zip ...
-	DEL premake-5.0.0-alpha15-windows.zip
+	DEL "premake-5.0.0-beta2-windows.zip"
+	DEL example.*
+	DEL luasocket.*
+	DEL premake5.pdb
 	ECHO.
 )
 
 IF EXIST "premake5.exe" (
 	ECHO Running premake5 ...
 	ECHO.
-	CALL premake5.exe vs2019
+	CALL premake5.exe vs2022
 	ECHO.
 ) ELSE (
 	ECHO Could not run premake5.exe, please get it manually from https://github.com/premake/premake-core/releases/
